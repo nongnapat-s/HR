c29f0c2e4985994bef2aeb257aa146e35d2b3bf6
Koramit Pichanaharee <koramit.p@gmail.com>
2018-08-29 09:56:00 +0700

    add more lists service

--- app/Http/Controllers/ListController.php
+++ app/Http/Controllers/ListController.php
@@ -2,18 +2,50 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Title;
+use App\Status;
 use App\SelectItem;
+use Illuminate\Http\Request;
+
 class ListController extends Controller
 {
+    /**
+     * Response List request
+     *
+     * @var string
+     */
     public function getList($name)
     {
-        if ($name == 'nation') {
-            $name = 'race';
+        switch ($name) {
+            case 'race':
+                $name = 'nation';
+            case 'nation':
+            case 'religion':
+            case 'blood_group':
+            case 'marital_status':
+                return SelectItem::select('value', 'label')
+                                 ->where('field', $name)
+                                 ->orderBy('order')
+                                 ->get();
+            case 'status':
+                return Status::select(
+                                    'id as value',
+                                    'name as label',
+                                    'active',
+                                    'category')
+                                    ->get();
+            case 'title':
+                return Title::select(
+                                    'id as value',
+                                    'name_short as label',
+                                    'name',
+                                    'name_short',
+                                    'name_eng',
+                                    'name_eng_short',
+                                    'gender')
+                                    ->get();
+            default:
+                return [];
         }
-        return SelectItem::select(['value', 'label'])
-                         ->where('field', $name)
-                         ->orderBy('order')
-                         ->get();
     }
 }
