6a2d30260378241ad43e565fbd90911a30347c15
nongnapat somdangdech <nongnapat.som@medicine.si.mahidol>
2018-09-04 14:11:51 +0700

    auto complete and npm update

--- resources/assets/js/components/inputs/AutoCompleteComponent.vue
+++ resources/assets/js/components/inputs/AutoCompleteComponent.vue
@@ -1,22 +1,30 @@
 <template>
   <div>
           <vue-bootstrap-typeahead
+                :data="data"
+                v-model="addressSearch"
+                size="lg"
+                placeholder="Type an address..."
+                @hit="selectedAddress = $event"
+          />
+
+          <!-- <vue-bootstrap-typeahead
                 :data="addresses"
                 v-model="addressSearch"
                 size="lg"
                 :serializer="s => s.text"
                 placeholder="Type an address..."
                 @hit="selectedAddress = $event"
-          />
+          /> -->
   </div>
 </template>
 
 <script>
     import _ from 'underscore'
-    import VueBootstrapTypeahead from '../inputs/VueBootstrapTypeahead'
+    import VueBootstrapTypeahead from 'vue-bootstrap-typeahead'
 
     // const API_URL = 'https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/suggest?f=json&maxSuggestions=5&text=:query'
-    const API_URL = '/get-list/postcode?search=:query '
+    const API_URL = '/get-list/postcode?search=:query'
     export default {
         components: {
             VueBootstrapTypeahead
@@ -25,18 +33,20 @@
         return {
             addressSearch: '',
             addresses: [],
-            selectedAddress: {}
+            selectedAddress: {},
+            data: ''
         }
     },
     methods: {
         async getAddresses(query) {
         const res = await fetch(API_URL.replace(':query', query))
         const suggestions = await res.json()
-        this.addresses = suggestions.suggestions
+        this.data = suggestions.map( function (postcode) { return postcode.location } )
+ 
         }
     },
     watch: {
-        addressSearch: _.debounce(function(addr) { this.getAddresses(addr) }, 1000)
+        addressSearch: _.debounce(function(addr) { this.getAddresses(addr) }, 300)
     }
 }
 </script>
