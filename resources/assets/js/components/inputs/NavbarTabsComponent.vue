d291f36e75b44355bc75db4e743362e609608676
nongnapat somdangdech <nongnapat.som@medicine.si.mahidol>
2018-10-12 08:46:47 +0700

    nav tabs component

---
+++ resources/assets/js/components/inputs/NavbarTabsComponent.vue
@@ -0,0 +1,26 @@
+<template>
+    <ul class="nav nav-tabs">
+        <li 
+            v-for ="(option,index) in options" 
+            :key = "index" 
+            :class = "option.class">
+            <a 
+                data-toggle="tab" 
+                :href="option.value"
+            >
+                <i 
+                    :class="option.icon"
+                ></i> 
+                    {{ option.label }}</a>
+        </li>
+   
+    </ul>
+</template>
+<script>
+export default {
+    props : {
+        options : { required : true },
+    }
+}
+</script>
+
