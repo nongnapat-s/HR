1ced0f03dd254c00542e18e736f4691539240457
Koramit Pichanaharee <koramit.p@gmail.com>
2018-10-19 09:31:00 +0700

    init ListManager

---
+++ app/Services/ListManager.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Lists\Title;
+use App\Models\Lists\Status;
+use App\Models\Lists\Postcode;
+use App\Models\Lists\Province;
+use App\Models\Lists\SelectItem;
+
+class ListManager
+{
+
+}
